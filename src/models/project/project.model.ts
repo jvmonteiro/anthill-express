ab1899c3f9376b78d957f2195e49d807e10f1759
Joao Vitor Monteiro Fernandes Correa <jvmonteirorj@gmail.com>
2020-12-10 17:50:10 -0300

    remove unused files

--- src/models/project/project.model.ts
+++
@@ -1,3 +0,0 @@
-import { ProjectSchema } from './project.schema';
-
-// export const Project = mongoose.model()
--- src/models/project/project.schema.ts
+++
@@ -1,6 +0,0 @@
-import { SchemaTypes, Schema } from 'mongoose';
-
-export const ProjectSchema: Schema = new Schema({
-  stakeholders: [SchemaTypes.ObjectId],
-  progress: SchemaTypes.Decimal128,
-});
