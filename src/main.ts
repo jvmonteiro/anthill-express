9e65ab33ace77eeea76d95c00d042eacde968fdc
Joao Vitor Monteiro Fernandes Correa <jvmonteirorj@gmail.com>
2020-12-10 17:49:24 -0300

    feat: implments routing for feature

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { server } from './server/server';
 
 const app = server();
-app.listen(3000, () => {
-  console.log('App listening');
+
+const port: Number = Number.parseInt(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { featRouter } from '../routes/feature.routes';
 export const server = () => {
   const app = express();
   app.use(helmet());
   app.use(cookieParser());
   app.use(bodyParser.json());
+
+  // Routes
+  app.use('/features', featRouter);
   return app;
 };
