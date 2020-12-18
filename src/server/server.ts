bca94c968a7d3149142eda0595dc61d2bb4fca57
Joao Vitor Monteiro Fernandes Correa <jvmonteirorj@gmail.com>
2020-12-18 13:10:57 -0300

    feat: enabling CORS for basic security

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,6 +5,16 @@ import bodyParser from 'body-parser';
 import { featRouter } from '../routes/feature.routes';
 export const server = () => {
   const app = express();
+
+  // Setup CORS
+  app.use((req, res, next) => {
+    res.set({
+      'Access-Control-Allow-Origin': ['http://localhost:8080', req.headers.origin],
+      'Access-Control-Allow-Headers': ['Origin', 'Authorization', 'Accept', 'Content-Type'],
+      'Access-Control-Allow-Methods': ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      'Access-Control-Allow-Credentials': 'true',
+    });
+  });
   app.use(helmet());
   app.use(cookieParser());
   app.use(bodyParser.json());
